894ab62f1a7bcaf2ce9e58246983d4ed6b6438cf
devrik99 <devrikoficial@gmail.com>
2023-12-12 14:45:24 -0500

    Se agrego el manifest pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [["@nuxtjs/tailwindcss", { viewer: false }], [
-    "@nuxtjs/color-mode",
-    {
-      preference: "light", // default value of $colorMode.preference
-      fallback: "light",
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
     },
-  ], "@vite-pwa/nuxt"],
+  },
+  modules: [
+    ["@nuxtjs/tailwindcss", { viewer: false }],
+    [
+      "@nuxtjs/color-mode",
+      {
+        preference: "light",
+        fallback: "light",
+      },
+    ],
+    [
+      "@vite-pwa/nuxt",
+      {
+        registerType: "autoUpdate",
+        manifest: {
+          name: "Culqi PWA",
+          short_name: "CulqiPWA",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "culqi-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "culqi-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "culqi-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      },
+    ],
+  ],
 });
